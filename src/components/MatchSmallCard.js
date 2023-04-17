68df1d31d522f879c1a832092bc2f775ca860ccb
Abhay <abhay34439@gmail.com>
2023-04-17 11:41:58 +0530

    completed the team ui

--- src/components/MatchSmallCard.js
+++ src/components/MatchSmallCard.js
@@ -1,16 +1,19 @@
 import {React} from "react"
 import { Link } from "react-router-dom";
+import './MatchSmallCard.scss';
 
 export const MatchSmallCard=({teamName,match})=>{
 
    const otherTeam = match.team1 ===teamName?match.team2:match.team1;
    const otr =`/teams/${otherTeam}`;
+   const isMatchwin = teamName===match.matchWinner;
     return (
-      <div className="MatchSmallCard">
-           <h2> 
-            vs  <Link to={otr}> {otherTeam}</Link> </h2>
+      <div  className={isMatchwin? 'MatchSmallCard won-card':'MatchSmallCard lost-card'}>
+
+        <span className="vs">vs</span>
+           <h2> <Link to={otr}> {otherTeam}</Link> </h2>
           
-            <p> {match.matchWinner} won by {match.resultMargin} {match.result} </p>
+            <p className="match-result"> {match.matchWinner} won by {match.resultMargin} {match.result} </p>
       </div>
     );
 }
